f93d73ce9f0ee7e0df653280d5046955a233de67
ivanseldas <ivanseldasp@gmail.com>
2024-06-08 17:05:51 +0200

    Level 4: 1-12 queries completed

--- SQL_assigment_questions_sqlite.sql
+++ SQL_assigment_questions_sqlite.sql
@@ -4,7 +4,7 @@
 SELECT COUNT(DISTINCT user_id) AS total_users_with_sessions FROM sessions;
 
 -- Question 2: Number of chargers used by user with id 1
-SELECT COUNT(charger_id) AS n_chargers_used FROM sessions
+SELECT COUNT(DISTINCT charger_id) AS n_chargers_used FROM sessions
 WHERE user_id = 1;
 
 
@@ -21,7 +21,7 @@ GROUP BY ChargerType;
 -- Question 4: Chargers being used by more than one user
 SELECT 
     charger_id,
-    COUNT(user_id) AS users_per_charger
+    COUNT(DISTINCT user_id) AS users_per_charger
 FROM sessions
 GROUP BY charger_id
 HAVING users_per_charger > 1;
@@ -29,10 +29,9 @@ HAVING users_per_charger > 1;
 -- Question 5: Average session time per charger
 SELECT 
     charger_id,
-    AVG(JULIANDAY(end_time) - JULIANDAY(start_time)) * 24 AS avg_session_hours
+    AVG(JULIANDAY(end_time) - JULIANDAY(start_time)) * 24 * 60 AS avg_session_min
 FROM sessions
-GROUP BY charger_id
-ORDER BY avg_session_hours DESC;
+GROUP BY charger_id;
     
 -- LEVEL 3
 
@@ -40,8 +39,7 @@ ORDER BY avg_session_hours DESC;
 WITH ChargerDayUser AS 
     (
     SELECT
-        u.name AS name,
-        u.surname AS surname,
+        u.name || ' ' || u.surname AS full_name,
         COUNT(s.user_id) AS chargers_used_day,
         strftime('%d', s.start_time) AS day
     FROM users u
@@ -49,14 +47,14 @@ WITH ChargerDayUser AS
     GROUP BY day, s.user_id
     HAVING chargers_used_day > 1
     )
-SELECT DISTINCT name, surname FROM ChargerDayUser;
+SELECT DISTINCT full_name FROM ChargerDayUser;
 
 -- Question 7: Top 3 chargers with longer sessions
 SELECT DISTINCT
     charger_id,
-    FIRST_VALUE(JULIANDAY(end_time) - JULIANDAY(start_time)) OVER (PARTITION BY charger_id) AS longest_session_hours
+    ((JULIANDAY(end_time) - JULIANDAY(start_time)) * 24 * 60) AS longest_session_min
 FROM sessions
-ORDER BY longest_session_hours DESC
+ORDER BY longest_session_min DESC
 LIMIT 3;
 
 -- Question 8: Average number of users per charger (per charger in general, not per charger_id specifically)
@@ -72,7 +70,7 @@ SELECT
 -- Question 9: Top 3 users with more chargers being used
 SELECT 
     user_id, 
-    COUNT(charger_id) AS ChargerUsed 
+    COUNT(DISTINCT charger_id) AS ChargerUsed 
 FROM sessions
 GROUP BY user_id
 ORDER BY ChargerUsed DESC
@@ -82,42 +80,54 @@ LIMIT 3;
 -- LEVEL 4
 
 -- Question 10: Number of users that have used only AC chargers, DC chargers or both
-WITH AC AS (
-        SELECT DISTINCT
-            COUNT(DISTINCT s.user_id) AS AC_users 
-        FROM sessions s
-        INNER JOIN chargers c ON s.charger_id = c.id
-        WHERE c.type = 'AC'
-    ),
-    DC AS (
-        SELECT DISTINCT
-            COUNT(DISTINCT s.user_id) AS DC_users
-        FROM sessions s
-        INNER JOIN chargers c ON s.charger_id = c.id
-        WHERE c.type = 'DC'
-    ),
-    BOTH AS (
-        SELECT DISTINCT
-            s.user_id
-            FROM sessions s
-            INNER JOIN chargers c ON s.charger_id = c.id
-            GROUP BY s.user_id
-            HAVING COUNT(DISTINCT c.type) = 2
-    )
 SELECT
-    AC_users,
-    DC_users,
-    COUNT(user_id) AS BOTH_users
-FROM AC, DC, BOTH;
+    s.user_id,
+    SUM(CASE WHEN c.type = 'AC' THEN 1 ELSE 0 END) AS AC_sum,
+    SUM(CASE WHEN c.type = 'DC' THEN 1 ELSE 0 END) AS DC_sum
+FROM sessions s
+INNER JOIN chargers c ON s.charger_id = c.id
+GROUP BY s.user_id;
+
+WITH UserCharger AS (
+    SELECT
+        s.user_id,
+        SUM(CASE WHEN c.type = 'AC' THEN 1 ELSE 0 END) AS AC_sum,
+        SUM(CASE WHEN c.type = 'DC' THEN 1 ELSE 0 END) AS DC_sum
+    FROM sessions s
+    INNER JOIN chargers c ON s.charger_id = c.id
+    GROUP BY s.user_id
+)
+SELECT 
+    SUM(CASE WHEN AC_sum > 0 AND DC_sum = 0 THEN 1 ELSE 0 END) AS AC_users,
+    SUM(CASE WHEN DC_sum > 0 AND AC_sum = 0 THEN 1 ELSE 0 END) AS DC_users,
+    SUM(CASE WHEN AC_sum > 0 AND DC_sum > 0 THEN 1 ELSE 0 END) AS BOTH_users
+FROM UserCharger;
 
 -- Question 11: Monthly average number of users per charger
-
-
+WITH UserChargerMonth AS (
+    SELECT 
+        charger_id,
+        COUNT(DISTINCT user_id) AS total_users,
+        strftime('%Y-%m', start_time) AS month
+    FROM sessions
+    GROUP BY month, charger_id
+)
+SELECT
+    charger_id,
+    month,
+    AVG(total_users) AS avg_users
+FROM UserChargerMonth
+GROUP BY charger_id, month;
 
 -- Question 12: Top 3 users per charger (for each charger, number of sessions)
-
-
-
+SELECT * FROM sessions;
+SELECT 
+    user_id,
+    COUNT(DISTINCT start_time) AS NumSessions
+FROM sessions
+GROUP BY user_id, charger_id
+ORDER BY NumSessions DESC
+LIMIT 5;
 
 -- LEVEL 5
 
